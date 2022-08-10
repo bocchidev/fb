313024eb82ed6362a4c49ccfc83d365848a58371
Sandy Pratama <96666448+arzxh@users.noreply.github.com>
2022-08-10 18:30:31 +0700

    Asfg

--- index.js
+++ index.js
@@ -6,13 +6,14 @@
 var axios = require('axios');
 var readline = require('readline');
 var cheerio = require('cheerio');
+var fs = require("fs").promises;
 
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('Masukkan url video Facebook: ', async (url) => {
+rl.question('Masukkan url: ', async (url) => {
     try {
         if (/(www|m).facebook/.test(url)) { 
             var mbasic = url.replace("www.facebook", "mbasic.facebook") || url.replace("m.facebook", "mbasic.facebook");
@@ -21,8 +22,8 @@ rl.question('Masukkan url video Facebook: ', async (url) => {
                 url: mbasic
             });
             var $ = cheerio.load(html.data);
-            var video = $('div[class="ca"]').children('a').attr('href'); 
-            var video = video.split('?src=')[1];
+            var video = $('div[class="cb"]').children('a').attr('href'); 
+            var video = video.split('?src=', 2)[1];
             var title = $('meta[property="og:title"]').attr("content");
             console.dir({
                 author: "Sandy Sayang Gura",
@@ -38,14 +39,14 @@ rl.question('Masukkan url video Facebook: ', async (url) => {
             });
             var _$ = cheerio.load(html_.data);
             var uri = _$('meta[property="og:url"]').attr("content");
-            var mbasic = uri.replace("www.facebook", "mbasic.facebook") || url.replace("m.facebook", "mbasic.facebook");
+            var mbasic = uri.replace("web.facebook", "mbasic.facebook");
             var html = await axios({
                 method: "get",
                 url: mbasic + '?extid=CL-UNK-UNK-UNK-AN_GK0T-GK1C-GK2T&refsrc=deprecated&ref=sharing&_rdr'
             });
             var $ = cheerio.load(html.data);
             var video = $('div[class="l"]').children("a").attr("href");
-            var video = video.split('?src=')[1];
+            var video = video.split('?src=', 2)[1];
             var title = $('meta[property="og:title"]').attr("content");
             console.dir({
                 author: "Sandy Sayang Gura",
@@ -53,6 +54,7 @@ rl.question('Masukkan url video Facebook: ', async (url) => {
                 title: title,
                 video: decodeURIComponent(video)
             });
+            //await fs.writeFile(Number(new Date) + '.txt', html.data);
         }
         else {
             console.table({
