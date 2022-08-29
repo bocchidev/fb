00426f8ad50ce0e2ed7642de5ae94f48bc2101fe
Sandy Pratama <96666448+arzxh@users.noreply.github.com>
2022-08-29 08:05:09 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /**
+ * hello 
  * 
  * 
  * 
@@ -6,23 +7,25 @@
  * 
  * 
  * 
- * 
-**/
+ **/
 const express = require("express");
-const axios = require("axios");
-const cheerio = require("cheerio");
-const bodyParser = require("body-parser");
-const main = require("./router/main.js");
-const facebook = require("./router/facebook.js");
-const PORT = process.env.PORT || 80;
+const session = require("express-session");
 const app = express();
-
+const main = require("./router/main.js");
+const PORT = process.env.PORT || 2006;
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static('public'));
+app.use(session({
+    secret: "Aku Cinta Gura"
+    , cookie: {
+        maxAge: 30000
+        , secure: true
+    }
+    , resave: false
+    , saveUninitialized: true
+}));
 app.use("/", main);
-app.use("/facebook", facebook);
-
 app.listen(PORT, () => {
-	console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Server listening on http://localhost:${PORT}`);
 });
